3eb46e8d0b11204491f8f89135fab9d1116870e2
CarlosFBlancasChavez <carlosblancas961@gmail.com>
2026-07-20 17:45:26 -0500

    Update Servicio.js

--- src/assets/modules/landing/domain/entities/Servicio.js
+++ src/assets/modules/landing/domain/entities/Servicio.js
@@ -1,9 +1,10 @@
 export class Servicio {
-    constructor({ id, titulo, descripcion, icono, imagen }) {
+    constructor({ id, titulo, descripcion, icono, imagen, encargado }) {
         this.id = id
         this.titulo = titulo
         this.descripcion = descripcion
         this.icono = icono
         this.imagen = imagen
+        this.encargado = encargado || null
     }
 }
