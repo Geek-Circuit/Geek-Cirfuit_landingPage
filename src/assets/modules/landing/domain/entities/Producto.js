59018e1ce346f1f02e041b849019393222440e5a
CarlosFBlancasChavez <carlosblancas961@gmail.com>
2026-07-20 17:45:09 -0500

    Update Producto.js

--- src/assets/modules/landing/domain/entities/Producto.js
+++ src/assets/modules/landing/domain/entities/Producto.js
@@ -1,9 +1,10 @@
 export class Producto {
-    constructor({ id, nombre, descripcion, imagen, precio }) {
+    constructor({ id, nombre, descripcion, imagen, precio, items }) {
         this.id = id
         this.nombre = nombre
         this.descripcion = descripcion
         this.imagen = imagen
         this.precio = precio
+        this.items = items || [] // productos individuales de esta categoría
     }
 }
